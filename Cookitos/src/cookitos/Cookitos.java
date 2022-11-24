c075776c8f09122a8b8a99c1acbd3caf06755fd9
Jose Manuel hernando <josemanuelhernando27@gmail.com>
2022-11-24 16:08:12 +0100

    ultimaprueba

--- Cookitos/src/cookitos/Cookitos.java
+++ Cookitos/src/cookitos/Cookitos.java
@@ -6,26 +6,7 @@ public class Cookitos {
 
     public static void main(String[] args) {
         System.out.println ("hola");
-		//esto es un comentario
-                //Muy buenas a todos
-                //esto es otra prueba de nuevo
-                //todo correcto
-
-<<<<<<< HEAD
-                //cambio1
-
-                
-                
-=======
-                //cambio
-
->>>>>>> 6ce6ebc152b9a8496529e727e0f76bf9222c5b78
-                //cambio 2
-
         System.out.println("Hola mundo");
-      
-
-
     }
     
 }
