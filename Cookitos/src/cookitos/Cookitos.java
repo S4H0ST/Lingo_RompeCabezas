2bb55a3f5f37de5df24c51ddb8b97798d6ba7217
Jose Manuel hernando <josemanuelhernando27@gmail.com>
2022-11-30 10:22:58 +0100

    v1.0

--- Cookitos/src/cookitos/Cookitos.java
+++ Cookitos/src/cookitos/Cookitos.java
@@ -5,17 +5,7 @@ package cookitos;
 public class Cookitos {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println ("hola");
 
-        System.out.println("Hola mundo");
-        //fdfdsffsfsdcdsfsdf
     }
-=======
-        
- 
-              
-    }    
->>>>>>> 22fe123b90548ebe8c93348da26e05d598bf883f
-}
+}    
 
