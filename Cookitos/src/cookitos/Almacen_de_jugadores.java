ab1b91b42ac4b787b636dcb287395b4e3cc0d293
laurasanchez99 <laurasanchez.urjc@gmail.com>
2022-11-30 12:15:22 +0100

    clases jugador y almacen

--- Cookitos/src/cookitos/Almacen_de_jugadores.java
+++ Cookitos/src/cookitos/Almacen_de_jugadores.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cookitos;
 
 /**
  *
- * @author laura
+ * @author sohaib, Jose, Laura
  */
 public class Almacen_de_jugadores {
     
