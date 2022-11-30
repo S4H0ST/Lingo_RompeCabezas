29a82a1e2a2a8026f083fff457de45702736579d
laurasanchez99 <laurasanchez.urjc@gmail.com>
2022-11-30 12:53:33 +0100

    jugador y almacen

--- Cookitos/src/cookitos/Jugador.java
+++ Cookitos/src/cookitos/Jugador.java
@@ -5,5 +5,24 @@ package cookitos;
  * @author sohaib, Jose, Laura
  */
 public class Jugador {
+    private String nombre;
+    private String contraseña;
     
+    public Jugador(String nom, String pass){
+        this.nombre = nom;
+        this.contraseña = pass;
+        
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getContraseña(){
+        return contraseña;
+    }
+    
+//    public class partidas_contra_otro(Jugador j){
+//        
+//    }
 }
